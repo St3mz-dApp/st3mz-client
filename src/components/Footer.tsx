cb68c5c42e04711db22b87a05d614fecaf38704c
Token-Factory-dApp <token.factory.dapp@protonmail.com>
2023-02-19 10:17:19 +0100

    Footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,16 +4,17 @@ import { TERMS_ROUTE, PRIVACY_ROUTE } from "../navigation/Routes";
 
 export const Footer = (): JSX.Element => {
   return (
-    <div className="flex flex-row items-center justify-around p-6 font-light">
+    <div className="flex flex-row items-center justify-around p-6">
       <div>
         <img src="/images/logo_192.png" className="inline h-10 pr-3"></img>
         <span>© {APP_NAME} 2023</span>
       </div>
-      <div>
+      <div className="flex">
         <div>
           <Link to={TERMS_ROUTE}>Terms</Link>
         </div>
-        <div className="pt-1">
+        <div className="mx-3 font-light">|</div>
+        <div>
           <Link to={PRIVACY_ROUTE}>Privacy</Link>
         </div>
       </div>
